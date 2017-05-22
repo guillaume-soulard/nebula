115fa58e57bbaed5c488b940b7f037649466c976
ogama <ogama47500@gmail.com>
2017-05-22 15:59:12 +0200

    Adding List type benchmark

--- nebula-benchmark/src/main/java/com/nebula/benchmark/NebulaBenchmark.java
+++ nebula-benchmark/src/main/java/com/nebula/benchmark/NebulaBenchmark.java
@@ -6,6 +6,7 @@ import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import com.nebula.benchmark.types.ListAmongItemsBenchmark;
+import com.nebula.benchmark.types.ListOfTypeBenchmark;
 
 public class NebulaBenchmark {
 
@@ -17,18 +18,18 @@ public class NebulaBenchmark {
 
 	private static String benchmarkClasses() {
 		StringBuilder builder = new StringBuilder();
-		// builder.append(OnePropertyOnEntityBenchmark.class.getSimpleName());
-		// builder.append("|");
-		// builder.append(TwoPropertyOnEntityBenchmark.class.getSimpleName());
-		// builder.append("|");
-		// builder.append(ThreePropertyOnEntityBenchmark.class.getSimpleName());
-		// builder.append("|");
-		// builder.append(FourPropertyOnEntityBenchmark.class.getSimpleName());
-		// builder.append("|");
-		// builder.append(FivePropertyOnEntityBenchmark.class.getSimpleName());
-		// builder.append("|");
-		// builder.append(ListOfTypeBenchmark.class.getSimpleName());
-		// builder.append("|");
+		builder.append(OnePropertyOnEntityBenchmark.class.getSimpleName());
+		builder.append("|");
+		builder.append(TwoPropertyOnEntityBenchmark.class.getSimpleName());
+		builder.append("|");
+		builder.append(ThreePropertyOnEntityBenchmark.class.getSimpleName());
+		builder.append("|");
+		builder.append(FourPropertyOnEntityBenchmark.class.getSimpleName());
+		builder.append("|");
+		builder.append(FivePropertyOnEntityBenchmark.class.getSimpleName());
+		builder.append("|");
+		builder.append(ListOfTypeBenchmark.class.getSimpleName());
+		builder.append("|");
 		builder.append(ListAmongItemsBenchmark.class.getSimpleName());
 		return builder.toString();
 	}
