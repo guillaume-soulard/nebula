5cb2cf479ceb76bb9e85b46c2c35e85e78ef5024
ogama <ogama47500@gmail.com>
2017-08-26 19:55:09 +0200

    Delete object project (moved to another branch)

--- nebula-object/src/main/java/com/nebula/object/fieldsetter/FieldSetter.java
+++
@@ -1,8 +0,0 @@
-package com.nebula.object.fieldsetter;
-
-import java.lang.reflect.Field;
-
-public interface FieldSetter {
-
-    void setValueToObjectForField(Object object, Field field, Object value);
-}
